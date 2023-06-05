360706e5247d3a049753eb72085e901c62293ffc
oy3o <mail@oy3o.com>
2023-06-05 22:47:35 +0800

    restore ";" of wgsl

--- lib.js
+++ lib.js
@@ -185,35 +185,31 @@ struct VertexInput {
     @location(0) position: vec2<f32>,
     @location(1) color: vec4<f32>,
 }
-
 struct VertexOutput {
     @builtin(position) position: vec4<f32>,
     @location(0) color: vec4<f32>,
 }
-
 @vertex
 fn main(input: VertexInput) -> VertexOutput {
-    var output: VertexOutput
-    output.position = vec4<f32>(input.position, 0.0, 1.0)
-    output.color = input.color
-    return output
+    var output: VertexOutput;
+    output.position = vec4<f32>(input.position, 0.0, 1.0);
+    output.color = input.color;
+    return output;
 }
 `
 
 const frag_wgsl = `
 struct FragmentInput {
     @location(0) color: vec4<f32>
-}
-
+};
 struct FragmentOutput {
     @location(0) color: vec4<f32>
-}
-
+};
 @fragment
 fn main(input: FragmentInput) -> FragmentOutput {
-    var output: FragmentOutput
-    output.color = input.color
-    return output
+    var output: FragmentOutput;
+    output.color = input.color;
+    return output;
 }
 `
 
